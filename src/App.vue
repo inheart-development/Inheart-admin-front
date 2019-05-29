dcefb1da3c1386f7cc86fdfbf6b4c817bb5215ab
김형규 <k7120792@gmail.com>
2019-05-29 22:39:12 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
