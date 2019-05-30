36a0e2efd330db7bf8c463d108b9bf5b4c4c5409
김형규 <k7120792@gmail.com>
2019-05-30 22:17:53 +0900

    Add signin page

---
+++ src/pages/signin.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="signin">
+    <h1>InHeart</h1>
+    <h2>관리자 로그인</h2>
+    <input class="input-box">
+    <input class="input-box">
+    <div>로그인</div>
+    <img :src="require('@/assets/logo.png')" :alt="'logo'">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "signin",
+  data() {
+    return {
+      id: "",
+      password: ""
+    };
+  }
+};
+</script>
+
+<style scoped>
+.sign-in {
+}
+</style>
