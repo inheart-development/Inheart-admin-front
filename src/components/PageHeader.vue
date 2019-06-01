7ff7ff13c326b9d9829312f1c59ed66ae60bcbf9
김형규 <k7120792@gmail.com>
2019-06-01 15:45:20 +0900

    Add PageHeader

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="page-header">
+    <img class="menu-icon" @click="openDrawer" src="../assets/ic_black_menu.png" alt="menu icon">
+    <h2 class="page-title">{{title}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'page-header',
+  props: ['title'],
+  methods: {
+    openDrawer() {
+      this.$emit('open-drawer');
+    },
+  },
+};
+</script>
+
+<style scoped>
+.page-header {
+  display: flex;
+  align-items: center;
+  padding: 1em 1em;
+}
+
+.menu-icon {
+  width: 2em;
+  height: 2em;
+}
+
+.page-title {
+  flex: 1;
+  color: #fff;
+  text-align: center;
+  margin: 0;
+}
+
+</style>
