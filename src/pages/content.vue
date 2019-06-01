e31e35306fdc077d9156c52cf044437c98a7f7e9
김형규 <k7120792@gmail.com>
2019-06-01 19:40:02 +0900

    Add CategoryButton to content page

--- src/pages/content.vue
+++ src/pages/content.vue
@@ -6,7 +6,17 @@
     />
     <div class="meditation-content-content">
       <page-header title="명상 컨텐츠 관리" @open-drawer="openDrawer" />
-      <div>
+      <div class="category-container">
+        <category-button
+          v-for="(category, index) in categories"
+          :key="index"
+          :isSelected="selectedCategory === index"
+          :category="category"
+          :categoryNo="index"
+          @select-category="selectCategory"
+        />
+      </div>
+      <div class="meditation-card-container">
       </div>
     </div>
   </div>
@@ -15,13 +25,16 @@
 <script>
 import navigationDrawer from '../components/NavigationDrawer.vue';
 import pageHeader from '../components/PageHeader.vue';
+import CategoryButton from '../components/utils/CategoryButton.vue';
 
 export default {
   name: 'content',
-  components: { navigationDrawer, pageHeader },
+  components: { navigationDrawer, pageHeader, CategoryButton },
   data() {
     return {
       isDrawerOpened: false,
+      categories: ['머리비우기', '마음비우기', '마음채우기', '글명상'],
+      selectedCategory: 0,
     };
   },
   methods: {
@@ -31,6 +44,9 @@ export default {
     closeDrawer() {
       this.isDrawerOpened = false;
     },
+    selectCategory(category) {
+      this.selectedCategory = category;
+    },
   },
 };
 </script>
@@ -39,5 +55,12 @@ export default {
 .meditation-content {
   height: 100vh;
   background: linear-gradient(#a7c4cd, #b4a0bd);
+}
+
+.category-container {
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+  padding: 0 1em;
+  grid-gap: 5px;
 }
 </style>
