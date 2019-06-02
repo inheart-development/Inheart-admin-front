51329457f19b760642dda795b4db17d211f266a6
김형규 <k7120792@gmail.com>
2019-06-02 22:51:08 +0900

    Add UserCardCombination, user api

--- src/pages/user.vue
+++ src/pages/user.vue
@@ -6,7 +6,13 @@
     />
     <div class="user-content">
       <page-header title="사용자 계정 관리" @open-drawer="openDrawer" />
-      <div>
+      <div class="user-card-container">
+        <user-card-combination
+          v-for="user in users"
+          :user="user"
+          :key="user.userNo"
+          @update="update"
+        />
       </div>
     </div>
   </div>
@@ -15,13 +21,17 @@
 <script>
 import navigationDrawer from '../components/NavigationDrawer.vue';
 import pageHeader from '../components/PageHeader.vue';
+import UserCardCombination from '../components/UserCardCombination.vue';
+
+import { getUsers } from '../lib/user';
 
 export default {
   name: 'user',
-  components: { navigationDrawer, pageHeader },
+  components: { navigationDrawer, pageHeader, UserCardCombination },
   data() {
     return {
       isDrawerOpened: false,
+      users: [],
     };
   },
   methods: {
@@ -31,13 +41,34 @@ export default {
     closeDrawer() {
       this.isDrawerOpened = false;
     },
+    async getUserData() {
+      try {
+        const response = await getUsers();
+        this.users = response.data.data;
+      } catch (e) {
+        // eslint-disable-next-line
+        console.log(e);
+      }
+    },
+    update() {
+      this.getUserData();
+    },
+  },
+  mounted() {
+    this.update();
   },
 };
 </script>
 
 <style scoped>
 .user {
-  height: 100vh;
+  min-height: 100vh;
   background: linear-gradient(#a7c4cd, #b4a0bd);
+}
+
+.user-card-container {
+  padding: 0 1em;
+  display: grid;
+  grid-gap: 1em;
 }
 </style>
