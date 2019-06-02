0afbd7476d4eaadd3c7b4b2ee171f59968ef669f
김형규 <k7120792@gmail.com>
2019-06-02 22:49:29 +0900

    Add UserCardCombination

---
+++ src/components/UserCardCombination.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <user-card
+      v-show="!isDetailOpen"
+      :user="user"
+      @open-detail="changeDetailState(true)"
+      @update="update"
+    />
+    <user-detail-card
+      v-show="isDetailOpen"
+      :num="user.userNo"
+      @close-detail="changeDetailState(false)"
+      @update="update"
+    />
+  </div>
+</template>
+
+<script>
+import UserCard from './UserCard.vue';
+import UserDetailCard from './UserDetailCard.vue';
+
+export default {
+  props: ['user'],
+  components: { UserCard, UserDetailCard },
+  data() {
+    return {
+      isDetailOpen: false,
+    };
+  },
+  methods: {
+    changeDetailState(value) {
+      this.isDetailOpen = value;
+    },
+    update() {
+      this.$emit('update');
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
