f765fce497bb808183115199393740f0324bc4ba
김형규 <k7120792@gmail.com>
2019-06-02 10:23:57 +0900

    Modify api name

--- src/lib/survey.js
+++ src/lib/survey.js
@@ -4,7 +4,7 @@ const baseUrl = 'http://54.180.153.125:3000/admin';
 
 export const getSurveys = () => axios.get(`${baseUrl}/survey/list`);
 
-export const ModifySurvey = ({
+export const modifySurvey = ({
   title,
   index,
 }) => axios.put(`${baseUrl}/survey`, {
