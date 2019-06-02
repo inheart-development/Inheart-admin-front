9667a76203eac91de8f23d537475f9cf99475722
김형규 <k7120792@gmail.com>
2019-06-02 23:14:56 +0900

    Add SurveyCard editable component, survey api

--- src/components/SurveyCard.vue
+++ src/components/SurveyCard.vue
@@ -2,7 +2,7 @@
   <div class="survey-card">
     <div class="content">
       <span class="index">{{index}}</span>
-      <span class="title" :class="{isEditable}" :contenteditable="isEditable">{{title}}</span>
+      <editable-component v-model="localTitle" :isEditable="isEditable" :value="localTitle" />
     </div>
     <img
       class="icon"
@@ -12,7 +12,7 @@
     >
     <img
       class="icon"
-      @click="changeEditable(false)"
+      @click="onModifySurvey"
       v-show="isEditable"
       src="../assets/ic_check.png"
       alt="check"
@@ -21,18 +21,48 @@
 </template>
 
 <script>
+import EditableComponent from './EditableComponent.vue';
+import { modifySurvey } from '../lib/survey';
+
 export default {
   name: 'survey-card',
+  components: { EditableComponent },
   props: ['title', 'index'],
   data() {
     return {
       isEditable: false,
+      localTitle: '',
     };
   },
   methods: {
     changeEditable(value) {
       this.isEditable = value;
     },
+    async onModifySurvey() {
+      // eslint-disable-next-line
+      if(confirm('게시물을 수정하시겠습니까?')) {
+        try {
+          await modifySurvey({ title: this.localTitle, index: this.index });
+          // eslint-disable-next-line
+          alert('게시물이 수정되었습니다.');
+          this.$emit('update');
+          this.changeEditable(false);
+        } catch (e) {
+          // eslint-disable-next-line
+          console.log(e);
+          // eslint-disable-next-line
+          alert('게시물이 수정되지 않았습니다.');
+        }
+      } else {
+        this.localTitle = this.title;
+        this.changeEditable(false);
+        // eslint-disable-next-line
+        alert('게시물이 수정이 취소되었습니다.');
+      }
+    },
+  },
+  created() {
+    this.localTitle = this.title;
   },
 };
 </script>
@@ -42,7 +72,7 @@ export default {
   display: flex;
   flex-direction: row;
   background: #fff;
-  padding: 0.5em 1em;
+  padding: 0.8em 1em;
   align-items: center;
 }
 
@@ -67,7 +97,7 @@ export default {
 }
 
 .icon {
-  width: 2em;
-  height: 2em;
+  width: 1.5em;
+  height: 1.5em;
 }
 </style>
