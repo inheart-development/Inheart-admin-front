bce81b7d6b44e39308cd4030656434c000ea7fdc
김형규 <k7120792@gmail.com>
2019-06-03 13:25:16 +0900

    Add content api to ContentPreiviewCard

--- src/components/ContentPreviewCard.vue
+++ src/components/ContentPreviewCard.vue
@@ -4,20 +4,40 @@
       <p class="title">{{contentTitle}}</p>
       <p class="explain">{{contentExplainEllipis}}</p>
     </div>
-    <img class="icon edit" src="../assets/ic_edit.png" alt="edit">
-    <img class="icon delete" src="../assets/ic_delete.png" alt="delete">
+    <img @click="modifyContent" class="icon edit" src="../assets/ic_edit.png" alt="edit">
+    <img @click="onDeleteContent" class="icon delete" src="../assets/ic_delete.png" alt="delete">
   </div>
 </template>
 
 <script>
 import { stringEllipsis } from '../utils';
+import { deleteContent } from '../lib/content';
 
 export default {
   name: 'content-preview-card',
   props: ['contentNo', 'contentTitle', 'contentExplain'],
   computed: {
     contentExplainEllipis() {
-      return stringEllipsis(this.contentExplain, 5);
+      return stringEllipsis(this.contentExplain, 18);
+    },
+  },
+  methods: {
+    modifyContent() {
+      this.$emit('modify-content', this.contentNo);
+    },
+    async onDeleteContent() {
+      // eslint-disable-next-line
+      if(confirm('컨텐츠를 삭제하시겠습니까?')) {
+        try {
+          await deleteContent({ contentsNo: this.contentNo });
+          this.$emit('update');
+          // eslint-disable-next-line
+          alert('컨텐츠가 삭제되었습니다.');
+        } catch (e) {
+          // eslint-disable-next-line
+          console.log(e);
+        }
+      }
     },
   },
 };
