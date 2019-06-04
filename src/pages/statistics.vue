be7cb5478e04a4fcd1e921865129c50cf48587c1
김형규 <k7120792@gmail.com>
2019-06-04 10:43:47 +0900

    Add LineChart, statistics api to page

--- src/pages/statistics.vue
+++ src/pages/statistics.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="statistics">
-    <navigation-drawer
-      v-show="isDrawerOpened"
-      @close-drawer="closeDrawer"
-    />
+    <navigation-drawer v-show="isDrawerOpened" @close-drawer="closeDrawer"/>
     <div class="statistics-content">
-      <page-header title="통계" @open-drawer="openDrawer" />
-      <div>
+      <page-header title="통계" @open-drawer="openDrawer"/>
+      <div class="statistics-container">
+        <div class="statistics-viewer">
+          <line-chart
+            :labels="labels"
+            :dataset="dataset"
+            :dataarray="datasetArray"
+            :options="options"
+          />
+        </div>
+        <div class="total-count">총 접속횟수: {{totalData}}회</div>
       </div>
     </div>
   </div>
@@ -15,13 +21,23 @@
 <script>
 import navigationDrawer from '../components/NavigationDrawer.vue';
 import pageHeader from '../components/PageHeader.vue';
+import { getDailyConnect, getTotalConnect } from '../lib/statistics';
+import LineChart from '../components/LineChart.vue';
 
 export default {
   name: 'statistics',
-  components: { navigationDrawer, pageHeader },
+  components: { navigationDrawer, pageHeader, LineChart },
   data() {
     return {
       isDrawerOpened: false,
+      dataset: {
+        label: '사용량',
+        backgroundColor: 'rgba(231,76,60, 0.3)',
+      },
+      datasetArray: [],
+      labels: [],
+      totalData: '',
+      options: { responsive: true, maintainAspectRatio: false },
     };
   },
   methods: {
@@ -31,6 +47,25 @@ export default {
     closeDrawer() {
       this.isDrawerOpened = false;
     },
+    async getDailyData() {
+      const response = await getDailyConnect();
+      const { data } = response.data;
+      const days = data.map(val => val.day.substring(0, 10));
+      const datasetData = data.map(val => val.count);
+      this.labels = days;
+      this.$set(this, 'datasetArray', datasetData);
+    },
+    async getTotalData() {
+      const response = await getTotalConnect();
+      this.totalData = response.data.data.count;
+    },
+    async updataData() {
+      this.getDailyData();
+      this.getTotalData();
+    },
+  },
+  created() {
+    this.updataData();
   },
 };
 </script>
@@ -39,5 +74,23 @@ export default {
 .statistics {
   height: 100vh;
   background: linear-gradient(#a7c4cd, #b4a0bd);
+}
+
+.statistics-container {
+  padding: 0 1em;
+}
+
+.statistics-viewer {
+  height: auto;
+  padding: 1em;
+  box-sizing: border-box;
+  background: #fff;
+}
+
+.total-count {
+  background: #fff;
+  text-align: center;
+  height: 3em;
+  line-height: 3em;
 }
 </style>
