112aa192d3c150ff57949c360067bbb70e00b0a1
김형규 <k7120792@gmail.com>
2019-06-04 10:43:22 +0900

    Add LineChart

---
+++ src/components/LineChart.vue
@@ -0,0 +1,42 @@
+<script>
+import { Line } from 'vue-chartjs';
+
+export default {
+  extends: Line,
+  props: ['labels', 'dataset', 'dataarray', 'options'],
+  mounted() {
+    this.renderLineChart();
+  },
+  data() {
+    return {
+      chartData: {},
+    };
+  },
+  methods: {
+    renderLineChart() {
+      this.renderChart(
+        {
+          labels: this.labels,
+          datasets: [{
+            label: this.dataset.label,
+            backgroundColor: this.dataset.backgroundColor,
+            data: this.dataarray,
+          }],
+        },
+        this.options,
+      );
+    },
+  },
+  watch: {
+    dataarray: {
+      handler() {
+        this.$set(this, 'chartData', this.dataarray);
+        this.renderLineChart();
+      },
+    },
+  },
+};
+</script>
+
+<style>
+</style>
