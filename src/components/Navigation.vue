a7bf6706406f89639e8e6cbf273a8cf65b6b2b91
khg0712 <k7120792@gmail.com>
2019-10-28 17:03:07 +0900

    Modify navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="navigation">
-    <div>
-      <img src="" alt="logo" />
+    <div class="logo-wrapper">
+      <img class="logo" src="../assets/admin_logo.png" alt="logo" />
+      <p>Inheart 관리자</p>
     </div>
     <div>
       <ul>
-        <li v-for="(link, index) in links" :key="index">
+        <li v-for="(link, index) in links" :key="index" :class="$route.path === `/${link.route}` ? 'active' : null">
           <router-link :to="link.route" class="navigator">
             {{link.name}}
           </router-link>
+          <img src="../assets/ic_keyboard_arrow_right.png" alt="arrow_right">
         </li>
       </ul>
     </div>
@@ -39,7 +41,7 @@ export default {
           name: 'FAQ 게시글 관리',
           route: 'faq'
         },
-      ]
+      ],
     };
   },
   methods: {
@@ -56,14 +58,45 @@ export default {
   width: 250px;
   background: #393c65;
   color: #fff;
+  padding: 0 20px;
+}
+
+.logo-wrapper {
+  height: 80px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.logo {
+  height: 40px;
 }
 
 li {
+  height: 45px;
+  display: flex;
+  align-items: center;
   list-style-type: none;
+  position: relative;
+  opacity: 0.7;
+}
+
+li.active, li:hover {
+  opacity: 1;
+}
+
+li > img {
+  position: absolute;
+  right: 0;
 }
 
 a {
   color: #fff;
   text-decoration: none;
 }
+
+a:hover {
+  opacity: 1;
+}
+
 </style>
