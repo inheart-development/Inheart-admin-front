9bc786615b1feaa3f3baee888ae12d058252ee18
khg0712 <k7120792@gmail.com>
2019-10-29 16:41:38 +0900

    Add TagButton, Editor

---
+++ src/components/TagButton.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="tag-button-wrapper">
+    <div class="button-list-item tag-button">{{content}}</div>
+    <div class="delete-button" @click="deleteTag(index)"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tag-button',
+  props: ['content', 'index'],
+  methods: {
+    deleteTag(key) {
+      this.$emit('deleteTag', key);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "../styles/style";
+
+  .tag-button-wrapper {
+    position: relative;
+    display: inline-block;
+    padding: 5px;
+  }
+
+  .selected-item {
+    background: #2C3E50 !important;
+    border: 2px solid #2C3E50 !important;
+    color: white !important;
+    box-shadow: 0 3px 6px 0 rgba(0, 0, 0, .3) !important;
+  }
+
+  .tag-button {
+    padding: 7px 10px;
+    border-radius: 16px;
+    border: 2px solid $theme-color;
+    box-shadow: 0 3px 9px -3px rgba(85, 103, 134, 0.35);
+    transition: all .4s ease-in-out;
+    font-weight: 300;
+    color: #3c4043;
+  }
+  
+  .delete-button {
+    position: absolute;
+    width: calc(100% - 10px);
+    height: calc(100% - 10px);
+    padding: 7px 10px;
+    border-radius: 16px;
+    border: 2px solid $theme-color;
+    box-shadow: 0 3px 9px -3px rgba(85, 103, 134, 0.35);
+    top: 5px;
+    left: 5px;
+    opacity: 0;
+    background-image: url('../assets/ic_clear.png');
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: 25px 25px;
+    background-color: #fff;
+    cursor: pointer;
+  }
+
+  .tag-button-wrapper:hover .delete-button {
+    opacity: 1;
+  }
+</style>
---
+++ src/components/TagButtonEditor.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="">
+    <div class="tag-container">
+      <tag-button 
+      v-for="(tag, index) in tags"
+      :content="tag"
+      :key="index"
+      :index="index"
+      @deleteTag="deleteTag" />
+    </div>
+    <div class="tag-input-container">
+      <input class="tag-input" :value="newTag" @change="changeNewTag" type="text" placeholder="태그를 입력해주세요."/>
+      <button class="submit-button" @click="createTag(newTag)">입력</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import TagButton from './TagButton';
+
+export default {
+  name: 'tag-button-editor',
+  components: {
+    TagButton
+  },
+  props: ['tags', 'newTag'],
+  methods: {
+    createTag(tag) {
+      this.$emit('createTag', tag);
+    },
+    changeNewTag(e) {
+      this.$emit('changeTag', e.target.value);
+    },
+    deleteTag(index) {
+      this.$emit('deleteTag', index);
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+.tag-container {
+  min-height: 46px;
+}
+
+.tag-input-container {
+  display: flex;
+}
+
+.tag-input {
+  flex: 1;
+  margin-right: 25px;
+  border-bottom: 1px solid #6d6e72;
+  background: none;
+  font-size: 1rem;
+  padding: 5px;
+}
+
+.submit-button {
+  position: relative;
+  padding: 7px 13px;
+  border-radius: 26px;
+  border: 2px solid #2C3E50;
+  background: #2C3E50;
+  color: #fff;
+  font-size: 1.3rem;
+}
+
+.submit-button:active {
+  top: 1px;
+}
+</style>
