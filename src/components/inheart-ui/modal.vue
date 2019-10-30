25d0003d5c3b55d0b3dbe79235275853f0ee5738
khg0712 <k7120792@gmail.com>
2019-10-30 10:18:03 +0900

    Modify modal

--- src/components/inheart-ui/modal.vue
+++ src/components/inheart-ui/modal.vue
@@ -1,25 +1,12 @@
 <template>
-  <div :class="modalClass" class="bg">
-    <!--    <div :style="modal_style" class="modal-container" ref="content">-->
-    <slot></slot>
-    <!--    </div>-->
+  <div class="bg">
+    <slot/>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'modal',
-    props: ['modal_style', 'is_open'],
-    data() {
-      return {};
-    },
-    computed: {
-      modalClass() {
-        return this.is_open ? 'show' : 'close';
-      }
-    },
-    mounted() {
-    }
+    name: 'modal'
   };
 </script>
 
@@ -35,25 +22,5 @@
     justify-content: center;
     align-items: center;
     z-index: 1
-  }
-
-  .modal-container {
-    background: white;
-    width: 20rem;
-    padding: 15px;
-    border: 1px solid #dee2e6;
-    border-radius: 10px;
-    box-shadow: 0 5px 12px -3px rgba(85, 103, 134, 0.35);
-    transition: all .4s ease-in-out;
-    z-index: 2
-  }
-
-  @keyframes modal-show {
-    0% {
-      opacity: 0
-    }
-    100% {
-      opacity: 1
-    }
   }
 </style>
