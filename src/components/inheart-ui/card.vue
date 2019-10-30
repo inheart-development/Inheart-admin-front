fc98e3328e34e1359dd0069e057d9511cb28145f
khg0712 <k7120792@gmail.com>
2019-10-30 11:38:51 +0900

    Modify card

--- src/components/inheart-ui/card.vue
+++ src/components/inheart-ui/card.vue
@@ -1,53 +1,40 @@
 <template>
-  <div :style="themeStyle" class="card flex-center">
-    <!--    <div class="inner-container flex-center">-->
+  <div @click="clicked" class="card">
     <slot></slot>
-    <!--    </div>-->
   </div>
 </template>
 
 <script>
   export default {
-    name: "card",
-    data() {
-      return {
-        themeStyle: {}
-      };
-    }, created() {
-      // setTimeout(() => {
-      //   window.$cardThemeQue.push(this);
-      //   window.$boxShadow.push(this);
-      //   const isLight = window.$theme === 'light';
-      //   this.$set(this.themeStyle, 'background', isLight ? '#fff' : '#212529');
-      //   this.$set(this.themeStyle, 'color', isLight ? '#343a40' : '#f8f9fa');
-      //   this.$set(this.themeStyle, 'boxShadow', isLight ? '0 5px 12px -3px rgba(85, 103, 134, 0.35)' : '0 5px 10px 0 rgba(0, 0, 0, .5)');
-      //
-      // }, 100);
+    name: 'card',
+    methods: {
+      clicked(event) {
+        this.$emit('click',event);
+      }
     }
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  @import "../../styles/style";
+
   .card {
+    @include flex-center;
     padding: 20px;
-    background: #fff;
-    height: 200px;
+    background: $background;
+    height: auto;
     margin: 20px auto 0;
     width: 600px;
     max-width: calc(100vw - 60px);
-    align-items: center;
-    justify-content: center;
-    box-shadow: 0 5px 12px -3px rgba(85, 103, 134, 0.35);
-    transition: all .4s ease-in-out;
-    display: flex;
-    border-radius: 8px;
-    animation: show .5s;
+    box-shadow: $box-shadow;
+    transition: $transition;
+    border-radius: $radius;
+    animation: show .7s;
   }
 
-  .title > h1 {
-    color: #6D6E72;
-    font-size: 2.5rem;
-    font-weight: 100;
-    text-align: center;
+  .dark .card {
+    box-shadow: $dark-box-shadow !important;
+    background: $dark-background !important;
+    color: $dark-color !important;
   }
 </style>
