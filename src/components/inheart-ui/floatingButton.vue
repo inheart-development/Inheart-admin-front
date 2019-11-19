a67bf2bd9241c347f5df18485a02ee2cd6df7992
JaeHeonShin <sjh0624@midasit.com>
2019-11-19 15:43:39 +0900

    Reduce the size of the floating button

--- src/components/inheart-ui/floatingButton.vue
+++ src/components/inheart-ui/floatingButton.vue
@@ -18,15 +18,15 @@
 <style scoped>
   .floating-button {
     background: #2C3E50;
-    height: 48px;
-    width: 48px;
+    height: 36px;
+    width: 36px;
     border-radius: 48px;
     box-shadow: 0 3px 10px 0 rgba(0, 0, 0, .3);
   }
 
   .floating-button img {
-    height: 36px;
-    width: 36px;
+    height: 24px;
+    width: 24px;
     padding: 4px 4px 4px 6px;
   }
 </style>
