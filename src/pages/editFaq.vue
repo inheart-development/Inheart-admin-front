c9e4c36ea61b0eedc6b299b63be4eb678bdf5cb3
JaeHeonShin <sjh0624@midasit.com>
2019-11-19 15:50:25 +0900

    update editFAQ design

--- src/pages/editFaq.vue
+++ src/pages/editFaq.vue
@@ -3,7 +3,6 @@
     <page-header :title="isOnModifying ? 'FAQ 수정' : 'FAQ 생성'"/>
     <div>
       <card class="edit-album-container">
-        <h2 class="title">Faq</h2>
         <div class="input-item">
           <label class="custom-label login-label" for="title">제목</label>
           <input
@@ -16,7 +15,7 @@
           />
         </div>
         <div class="input-item">
-          <label class="custom-label login-label" for="explain">내용</label>
+          <label class="custom-label login-label" for="content">내용</label>
           <textarea
             class="custom-input"
             id="content"
@@ -27,11 +26,11 @@
           />
         </div>
       </card>
-      <card v-if="!isOnModifying" class="btn-card" @click="updateAddFaq">업로드</card>
-      <card v-if="isOnModifying" class="btn-card" @click="updateModifyFaq">업로드</card>
-      <router-link to="/faq">
-        <card class="btn-card">닫기</card>
-      </router-link>
+      <div class="inline-block">
+        <card class="button-card" @click="$router.push('/faq')">취소</card>
+        <card v-if="!isOnModifying" class="button-card" @click="updateAddFaq">업로드</card>
+        <card v-if="isOnModifying" class="button-card" @click="updateModifyFaq">업로드</card>
+      </div>
     </div>
   </div>
 </template>
@@ -39,11 +38,12 @@
 <script>
   import Card from '../components/inheart-ui/card';
   import pageHeader from '../components/PageHeader.vue';
-  import { addFaq, getDetailFaq, modifyFaq } from '../lib/faq';
+  import {addFaq, getDetailFaq, modifyFaq} from '../lib/faq';
+
   export default {
     name: 'edit-faq',
     props: ['faqNo'],
-    components: { Card, pageHeader },
+    components: {Card, pageHeader},
     data() {
       return {
         faqQuestion: '',
@@ -58,7 +58,7 @@
     methods: {
       async updateFaq(faqNo) {
         const response = await getDetailFaq(faqNo);
-        const { data } = response;
+        const {data} = response;
         this.faqQuestion = data.data.faqQuestion;
         this.faqAnswer = data.data.faqAnswer;
       },
@@ -79,40 +79,59 @@
       },
     },
     mounted() {
-      if(this.isOnModifying) this.updateFaq(this.faqNo);
+      if (this.isOnModifying) this.updateFaq(this.faqNo);
     },
   };
 </script>
 
-<style scoped>
-.container {
-  max-height: 100vh;
-  overflow: auto;
-  flex-direction: column;
-  padding: 10px 10px 30px 10px;
-}
+<style lang="scss" scoped>
+  .inline-block {
+    max-width: 640px;
+    margin: 0 auto;
+    display: flex;
+  }
 
-.edit-album-container {
-  flex-direction: column;
-  align-items: flex-start;
-}
+  .button-card {
+    width: calc(50% - 30px);
+    padding: 10px;
+    user-select: none;
+    cursor: pointer;
+    &:first-child {
+      color:#e03131;
+    }
+    &:last-child {
+      margin-left: 0;
+    }
+  }
 
-.input-item {
-  width: 100%;
-  margin: 15px 15px 15px 0;
-}
+  .container {
+    max-height: 100vh;
+    overflow: auto;
+    flex-direction: column;
+    padding: 10px 10px 30px 10px;
+  }
 
-.custom-input {
-  border: 0;
-  border-bottom: 1px solid #6d6e72;
-  background: none;
-  font-size: 1rem;
-  padding: 5px;
-  width: 100%;
-  font-weight: 300;
-}
+  .edit-album-container {
+    flex-direction: column;
+    align-items: flex-start;
+  }
 
-textarea {
+  .input-item {
+    width: 100%;
+    margin: 15px 15px 15px 0;
+  }
+
+  .custom-input {
+    border: 0;
+    border-bottom: 1px solid #6d6e72;
+    background: none;
+    font-size: 1rem;
+    padding: 5px;
+    width: 100%;
+    font-weight: 300;
+  }
+
+  textarea {
     resize: none;
   }
 </style>
