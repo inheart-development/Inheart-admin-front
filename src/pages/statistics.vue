11033473fae87e7402f184634fc20c0e387ff682
JaeHeonShin <sjh0624@midasit.com>
2019-11-19 16:00:56 +0900

    통계페이지 업데이트

--- src/pages/statistics.vue
+++ src/pages/statistics.vue
@@ -2,18 +2,14 @@
   <div class="statistics">
     <div class="statistics-content">
       <page-header title="통계"/>
-      <div class="statistic-list">
+      <div class="inline-block">
         <card class="counter">
-          <div class="inner-container">
-            <h3>총 접속</h3>
-            <h2>{{totalCnt}} 회</h2>
-          </div>
+          <h3>총 접속</h3>
+          <h2>{{totalCnt}} 회</h2>
         </card>
         <card class="counter">
-          <div class="inner-container">
-            <h3>일평균 접속</h3>
-            <h2>{{avgCount}} 회</h2>
-          </div>
+          <h3>일평균 접속</h3>
+          <h2>{{avgCount}} 회</h2>
         </card>
       </div>
       <chart-viewer
@@ -29,130 +25,130 @@
 </template>
 
 <script>
-import moment from 'moment'; 
-import pageHeader from '../components/PageHeader.vue';
-import { getYearlyStatistics, getMonthlyStatistics, getdailyStatistics, getConnectionCount } from '../lib/statistics';
-import LineChart from '../components/LineChart.vue';
-import Card from '../components/inheart-ui/card';
-import ChartViewer from '../components/ChartViewer';
+  import moment from 'moment';
+  import pageHeader from '../components/PageHeader.vue';
+  import {getYearlyStatistics, getMonthlyStatistics, getdailyStatistics, getConnectionCount} from '../lib/statistics';
+  import LineChart from '../components/LineChart.vue';
+  import Card from '../components/inheart-ui/card';
+  import ChartViewer from '../components/ChartViewer';
 
-export default {
-  name: 'statistics',
-  components: {
-    Card,
-    pageHeader,
-    LineChart,
-    ChartViewer
-  },
-  data() {
-    return {
-      dataset: {
-        label: '사용량',
-        backgroundColor: 'rgba(231,76,60, 0.3)',
+  export default {
+    name: 'statistics',
+    components: {
+      Card,
+      pageHeader,
+      LineChart,
+      ChartViewer
+    },
+    data() {
+      return {
+        dataset: {
+          label: '사용량',
+          backgroundColor: 'rgba(231,76,60, 0.3)',
+        },
+        datasetArray: [],
+        labels: [],
+        totalCnt: 0,
+        avgCount: 0,
+        currentPoint: moment(),
+        showingPoint: moment().format('YYYY'),
+        selectedStatistics: 0,
+      };
+    },
+    watch: {
+      selectedStatistics() {
+        this.updateSwitcher(this.selectedStatistics);
+      }
+    },
+    methods: {
+      changeStatisticsType(index) {
+        this.selectedStatistics = index;
       },
-      datasetArray: [],
-      labels: [],
-      totalCnt: 0,
-      avgCount: 0,
-      currentPoint: moment(),
-      showingPoint: moment().format('YYYY'),
-      selectedStatistics: 0,
-    };
-  },
-  watch: {
-    selectedStatistics() {
+      getDailyLabel(index) {
+        if (index >= 9) return `${index}시`
+        else return `0${index}시`
+      },
+      getMonthlyLabel(index) {
+        if (index >= 9) return `${index + 1}일`
+        else return `0${index + 1}일`
+      },
+      getYearlyLabel(index) {
+        if (index >= 9) return `${index + 1}월`
+        else return `0${index + 1}월`
+      },
+      changeCurrentPoint(coefficient) {
+        switch (this.selectedStatistics) {
+          case 0:
+            this.currentPoint = Object.assign(this.currentPoint.add(coefficient, 'y'));
+            break;
+          case 1:
+            this.currentPoint = Object.assign(this.currentPoint.add(coefficient, 'M'));
+            break;
+          case 2:
+            this.currentPoint = Object.assign(this.currentPoint.add(coefficient, 'd'));
+            break;
+          default:
+            break;
+        }
+        this.updateSwitcher(this.selectedStatistics)
+      },
+      updateSwitcher(statisticsNum) {
+        switch (statisticsNum) {
+          case 0:
+            this.showingPoint = this.currentPoint.format('YYYY');
+            this.updateYearlyStatistics(this.showingPoint);
+            return;
+          case 1:
+            this.showingPoint = this.currentPoint.format('YYYY-MM')
+            this.updateMonthlyStatistics(this.showingPoint);
+            return;
+          case 2:
+            this.showingPoint = this.currentPoint.format('YYYY-MM-DD')
+            this.updateDailyStatistics(this.showingPoint);
+            return;
+          default:
+            return;
+        }
+      },
+      async updateDailyStatistics(date) {
+        const response = await getdailyStatistics(date);
+        const {data} = response.data;
+        this.labels = data.map((val, i) => this.getDailyLabel(i));
+        this.datasetArray = data
+      },
+      async updateMonthlyStatistics(date) {
+        const response = await getMonthlyStatistics(date);
+        const {data} = response.data;
+        this.labels = data.map((val, i) => this.getMonthlyLabel(i));
+        this.datasetArray = data;
+      },
+      async updateYearlyStatistics(date) {
+        const response = await getYearlyStatistics(date);
+        const {data} = response.data;
+        this.labels = data.map(((val, i) => this.getYearlyLabel(i)));
+        this.datasetArray = data;
+      },
+      async updateConnectCount() {
+        const response = await getConnectionCount();
+        const {data} = response;
+        this.totalCnt = data.data.totalCnt;
+        this.avgCount = data.data.avgCount;
+      }
+    },
+    mounted() {
+      console.dir(moment().add(1, 'd'))
       this.updateSwitcher(this.selectedStatistics);
-    }
-  },
-  methods: {
-    changeStatisticsType(index) {
-      this.selectedStatistics = index;
+      this.updateConnectCount();
     },
-    getDailyLabel(index) {
-      if(index >= 9) return `${index}시`
-      else return `0${index}시`
-    },
-    getMonthlyLabel(index) {
-      if(index >= 9) return `${index+1}일`
-      else return `0${index+1}일`
-    },
-    getYearlyLabel(index) {
-      if(index >= 9) return `${index+1}월`
-      else return `0${index+1}월`
-    },
-    changeCurrentPoint(coefficient) {
-      switch(this.selectedStatistics) {
-        case 0:
-          this.currentPoint = Object.assign(this.currentPoint.add(coefficient, 'y'));
-          break;
-        case 1:
-          this.currentPoint = Object.assign(this.currentPoint.add(coefficient, 'M'));
-          break;
-        case 2:
-          this.currentPoint = Object.assign(this.currentPoint.add(coefficient, 'd'));
-          break;
-        default:
-          break;
-      }
-      this.updateSwitcher(this.selectedStatistics)
-    },
-    updateSwitcher(statisticsNum) {
-      switch(statisticsNum) {
-        case 0:
-          this.showingPoint = this.currentPoint.format('YYYY');
-          this.updateYearlyStatistics(this.showingPoint);
-          return;
-        case 1:
-          this.showingPoint = this.currentPoint.format('YYYY-MM')
-          this.updateMonthlyStatistics(this.showingPoint);
-          return;
-        case 2:
-          this.showingPoint = this.currentPoint.format('YYYY-MM-DD')
-          this.updateDailyStatistics(this.showingPoint);
-          return;
-        default:
-          return;
-      }
-    },
-    async updateDailyStatistics(date) {
-      const response = await getdailyStatistics(date);
-      const { data } = response.data;
-      this.labels = data.map((val, i) => this.getDailyLabel(i));
-      this.datasetArray = data
-    },
-    async updateMonthlyStatistics(date) {
-      const response = await getMonthlyStatistics(date);
-      const { data } = response.data;
-      this.labels = data.map((val, i) => this.getMonthlyLabel(i));
-      this.datasetArray = data;
-    },
-    async updateYearlyStatistics(date) {
-      const response = await getYearlyStatistics(date);
-      const { data } = response.data;
-      this.labels = data.map(((val, i) => this.getYearlyLabel(i)));
-      this.datasetArray = data;
-    },
-    async updateConnectCount() {
-      const response = await getConnectionCount();
-      const { data } = response;
-      this.totalCnt = data.data.totalCnt;
-      this.avgCount = data.data.avgCount;
-    }
-  },
-  mounted() {
-    console.dir(moment().add(1, 'd'))
-    this.updateSwitcher(this.selectedStatistics);
-    this.updateConnectCount();
-  },
-};
+  };
 </script>
 
 <style scoped>
-  .statistic-list {
-    width: 600px;
+
+  .inline-block {
+    max-width: 640px;
     margin: 0 auto;
     display: flex;
-    justify-content: space-around;
   }
 
   .statistic-list .counter {
@@ -160,10 +156,11 @@ export default {
   }
 
   .counter {
+    flex-direction: column;
     height: 120px;
-    width: 250px;
+    width: calc(50% - 30px);
     margin: 20px;
-    justify-content: flex-start;
+    text-align: center;
   }
 
   .counter h3 {
@@ -176,26 +173,9 @@ export default {
     font-weight: 100;
   }
 
-  .statistics-card {
-    height: auto;
-  }
-
   .statistics {
     height: 100vh;
     overflow: auto;
     padding-bottom: 50px;
-  }
-
-  .statistics-container {
-    position: relative;
-    width: 100%;
-    margin: 0;
-  }
-
-  .statistics-viewer {
-    height: auto;
-    padding: 1em;
-    box-sizing: border-box;
-
   }
 </style>
